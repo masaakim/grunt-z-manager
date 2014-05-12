0ff84b514292e42f6a6fd8ea25dea47cfcf75878
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-13 00:57:15 +0900

    fix Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,10 +9,8 @@
 'use strict';
 
 module.exports = function (grunt) {
-  // load all npm grunt tasks
   require('load-grunt-tasks')(grunt);
 
-  // Project configuration.
   grunt.initConfig({
     jshint: {
       all: [
@@ -26,46 +24,28 @@ module.exports = function (grunt) {
       }
     },
 
-    // Before generating any new files, remove any previously-created files.
     clean: {
       tests: ['tmp']
     },
 
-    // Configuration to be run (and then tested).
     z_manager: {
-      default_options: {
-        options: {
-        },
+      adapt: {
         files: {
-          'tmp/default_options': ['test/fixtures/testing', 'test/fixtures/123']
+          'tmp/out.css': ['test/fixtures/z-manager.css']
         }
       },
-      custom_options: {
-        options: {
-          separator: ': ',
-          punctuation: ' !!!'
-        },
+      create: {
         files: {
-          'tmp/custom_options': ['test/fixtures/testing', 'test/fixtures/123']
+          './.zmanagerc': ['test/fixtures/z-manager.css']
         }
       }
-    },
-
-    // Unit tests.
-    nodeunit: {
-      tests: ['test/*_test.js']
     }
 
   });
 
-  // Actually load this plugin's task(s).
   grunt.loadTasks('tasks');
 
-  // Whenever the "test" task is run, first clean the "tmp" dir, then run this
-  // plugin's task(s), then test the result.
-  grunt.registerTask('test', ['clean', 'z_manager', 'nodeunit']);
-
-  // By default, lint and run all tests.
+  grunt.registerTask('test', ['clean', 'z_manager']);
   grunt.registerTask('default', ['jshint', 'test']);
 
 };
